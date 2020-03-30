e5cde9e138f401a77ec9d9a43dda86f0fff47d91
Dominik Helleberg <dominik.helleberg@inovex.de>
2020-03-31 00:07:24 +0200

    added LED animation

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,30 @@ int volume = 20;
 #define PIXEL_COUNT 12
 CRGB leds[PIXEL_COUNT];
 
+const int MIN_DISTANCE = 15;
+
+const int MODE_INIT_ANIM = 1;
+const int MODE_COUNTER = 2;
+const int MODE_OFF = 0;
+int mode = MODE_OFF;
+
+int dimFactor = 0;
+boolean dimDown = true;
+int dimCounter = 0;
+
+long washStarted = 0;
+int counter = 0;
+const long WASH_TIME = 20 * 1000; // 20sec
+
 UltraSonicDistanceSensor distanceSensor(15, 4);
+
+void leds_off() {
+    for (int i = 0; i < PIXEL_COUNT; i++)
+    { // For each pixel...
+      leds[i] = CRGB::Black;
+      FastLED.show();   // Send the updated pixel colors to the hardware.
+    }
+}
 
 void setup()
 {
@@ -33,18 +56,90 @@ void setup()
   dfPlayer.volume(volume); // Set volume value (0~30).
   dfPlayer.EQ(DFPLAYER_EQ_NORMAL);
   dfPlayer.outputDevice(DFPLAYER_DEVICE_SD);
+  leds_off();
+}
+
+void initLEDs() {
+    for (int i = 0; i < PIXEL_COUNT; i++)
+    { // For each pixel...
+      leds[i].setRGB(255, 30, 0);
+      FastLED.show();    
+    }
+    dimFactor = 255;
+    dimDown = true;
+    dimCounter = 0;
+}
+
+void doFadeAnim() { 
+  Serial.printf("dimFactor: %d\n",dimFactor);
+  if(dimDown)
+    dimFactor = dimFactor - 20;
+  else
+    dimFactor = dimFactor + 20;
+  if(dimFactor < 20) {
+    dimFactor = 20;
+    dimDown = false;
+  }
+  else {
+    if(dimFactor > 255) {
+      dimCounter = dimCounter + 1;
+      dimDown = true;
+      dimFactor = 255;
+      if(dimCounter == 2) {
+        mode = MODE_COUNTER;
+        leds_off();
+      }
+          
+    }
+  }
+  for (int i = 0; i < PIXEL_COUNT; i++)
+  { // For each pixel...
+    leds[i].setRGB(255, 30, 0);
+    leds[i].fadeToBlackBy(dimFactor);
+    FastLED.show();    
+  }
+  delay(10);
+}
+
+void doClockCount() {
+  //calc counter
+  long remainingSecs = (washStarted + WASH_TIME) - millis();
+  Serial.printf("remaining ms: %d\n",remainingSecs);
+
+  if(remainingSecs <= 0) {
+    leds_off();
+    mode = MODE_OFF;
+  }
+    
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
-  Serial.println(distanceSensor.measureDistanceCm());
-  delay(500);
-  for (int i = 0; i < PIXEL_COUNT; i++)
-  { // For each pixel...
-    leds[i].setRGB(255,30,0);
-    FastLED.show();
-    delay(100);
+  switch (mode)
+  {
+  case MODE_OFF: {
+    int distance = distanceSensor.measureDistanceCm();
+    Serial.println(distance);
+    if (distance < MIN_DISTANCE && distance > -1)
+    {
+      mode = MODE_INIT_ANIM;
+      initLEDs();
+      washStarted = millis();
+      dfPlayer.play(1);
+      break;
+    }
+    else
+      delay(500);
+    break;
+  }
+  case MODE_INIT_ANIM:
+    doFadeAnim();    
+    break;
+  case MODE_COUNTER:
+    doClockCount();
+    break;
+
   }
   
   //dfPlayer.play(1);
